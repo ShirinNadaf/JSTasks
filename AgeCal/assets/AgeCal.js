b5eaff952fafb48ff4f4bc0fdf99b5d551c03ab0
ShirinNadaf <shirin.nadaf@successive.tech>
2021-11-10 18:02:44 +0530

    tasks

---
+++ AgeCal/assets/AgeCal.js
@@ -0,0 +1,13 @@
+
+function ageCal(){
+    var currentYear = 2021;
+    var bdate= 1996; 
+    var age = (currentYear - bdate)
+      return age;
+}
+
+console.log(ageCal())
+
+
+
+
---
+++ BMI_Cal/assets/bmical.js
@@ -0,0 +1,8 @@
+function BMI(){
+    var wight=63;
+    var height = 3.5;
+    let bmi = wight/(height**2);
+    return bmi;
+}
+var bmi = console.log(BMI());
+ 
