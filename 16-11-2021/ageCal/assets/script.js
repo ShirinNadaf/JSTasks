654f6ceb7eb5c0426d6062db6b048162196fdc93
ShirinNadaf <shirin.nadaf@successive.tech>
2021-11-17 09:54:29 +0530

    completed

---
+++ 16-11-2021/ageCal/assets/script.js
@@ -0,0 +1,10 @@
+let ageCal={
+    age : function(age) {
+     if(age >=18){
+         document.write('age is major ')
+     }else{
+         document.write('age is minor')
+     }
+   }
+ }
+  console.log(ageCal.age(20))
